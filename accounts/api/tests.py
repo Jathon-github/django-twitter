ba9d6691ae86ea07e178400ec66e99bf3d0857a7
Jathon <1539256023@qq.com>
2021-05-15 23:07:28 +0800

    Add tweets api and tests

--- accounts/api/tests.py
+++ accounts/api/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from testing.testcases import TestCase
 from rest_framework.test import APIClient
 from django.contrib.auth.models import User
 
@@ -12,14 +12,11 @@ LOGIN_STATUS_URL = '/api/accounts/login_status/'
 class AccountApiTests(TestCase):
     def setUp(self):
         self.client = APIClient()
-        self.user = self.createUser(
+        self.user = self.create_user(
             username='admin',
             email='admin@django.com',
             password='correct password',
         )
-
-    def createUser(self, username, email, password):
-        return User.objects.create_user(username, email, password)
 
     def test_login(self):
         response = self.client.get(LOGIN_URL, {
