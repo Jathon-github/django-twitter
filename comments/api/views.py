f139928bb195222e025c83cfbdc31a4d5748c634
Jathon <1539256023@qq.com>
2021-05-26 22:58:24 +0800

    Create api for comments

--- comments/api/views.py
+++ comments/api/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from comments.api.serializers import (
+    CommentSerializerForCreate,
+    CommentSerializer,
+)
+
 
 # Create your views here.
+class CommentViewSet(viewsets.GenericViewSet):
+    serializer_class = CommentSerializerForCreate
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return [IsAuthenticated()]
+        return [AllowAny()]
+
+    def create(self, request):
+        data = {
+            'user_id': request.user.id,
+            'tweet_id': request.data.get('tweet_id'),
+            'content': request.data.get('content'),
+        }
+
+        serializer = CommentSerializerForCreate(data=data)
+        if not serializer.is_valid():
+            return Response({
+                'message': 'Please check input.',
+                'errors': serializer.errors
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        comment = serializer.save()
+        return Response({
+            'comment': CommentSerializer(comment).data,
+        }, status=status.HTTP_201_CREATED)
