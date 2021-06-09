f1f0ab94d6c314df419a65563a7d79e1a7de05c0
Jathon <1539256023@qq.com>
2021-06-09 17:14:44 +0800

    Support cancel a like

--- likes/api/views.py
+++ likes/api/views.py
@@ -1,6 +1,11 @@
-from likes.api.serializers import LikeSerializer, LikeSerializerForCreate
+from likes.api.serializers import (
+    LikeSerializer,
+    LikeSerializerForCancel,
+    LikeSerializerForCreate,
+)
 from likes.models import Like
 from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from utils.decorators import required_params
@@ -28,3 +33,23 @@ class LikeViewSet(viewsets.GenericViewSet):
         return Response({
             'like': LikeSerializer(instance).data,
         }, status=status.HTTP_201_CREATED)
+
+    @action(methods=['POST'], detail=False)
+    @required_params(request_attr='data', params=['content_type', 'object_id'])
+    def cancel(self, request):
+        serializer = LikeSerializerForCancel(
+            data=request.data,
+            context={'request': request},
+        )
+
+        if not serializer.is_valid():
+            return Response({
+                'message': 'Please check input.',
+                'errors': serializer.errors
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        deleted = serializer.cancel()
+        return Response({
+            'deleted': deleted,
+            'success': True,
+        }, status=status.HTTP_200_OK)
